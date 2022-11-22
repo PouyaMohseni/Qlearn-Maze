94d9e15cc93cdde49e78dc2e979cf07370ced17f
Pouya Mohseni <93826984+PouyaMohseni@users.noreply.github.com>
2022-11-22 17:47:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from maze import maze
 
+maze = maze([
+    ["W","W","B","B","B"],
+    ["B","W","W","B","B"],
+    ["B","W","W","W","W"],
+    ["B","W","W","W","W"],
+    ["B","W","B","B","W"],
+],[0,0],[4,4])
+
+maze.show_env()
+
+maze.train(10,0.2,0.2)
